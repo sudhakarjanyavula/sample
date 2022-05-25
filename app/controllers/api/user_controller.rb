3b0bd1828381d13a199130a47fe5ebaace41b897
sudhakar <sudhakarjanyavula@gmail.com>
2022-05-25 03:36:20 -0400

    first commit

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,40 @@
+class Api::UserController < ApplicationController
+	def create
+		render json: User.create(user_params)
+	end
+
+	def index
+		render json: User.paginate(:page => params[:page])
+	end
+
+	def update
+		user = User.find_by_id(params[:id])
+		user.update(user_params) if user
+		render json: user
+	end
+
+	def show
+		render json: User.find_by_id(params[:id])
+	end
+
+	def destroy
+		User.delete(params[:id])
+		render jsaon: []
+	end
+
+	def typeahead
+		matches = []
+		input_str = params[:input]
+		User.type_matches(input_str).each do |user|
+			matches << user.firstName + ' ' + user.lastName
+		end
+		results = matches.present? ? matches.join( 'and' ) : ''
+		render json: results
+	end
+
+	private 
+
+	def user_params
+		params.permit(:firstName, :lastName, :email)
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	self.per_page = 3
+	 def self.type_matches str
+	 	User.where('firstName LIKE ? or lastName LIKE ? or email LIKE ?',
+	 		"%#{str}%","%#{str}%","%#{str}%").all
+	 end
+end
